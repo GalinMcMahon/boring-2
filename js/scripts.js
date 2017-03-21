1455c110fc0dd5f1802d85c663f0ad8d4699be54
Galin McMahon and Mohamed Warsame <galinmcmahon@gmail.com>
2017-03-21 13:06:16 -0700

    Add js for borders around paragraphs.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".pborder").click(function() {
+    $(".pborder").removeClass("border1");
+    $(".pborder").addClass("border1");
+  });
+});
+
+$(document).ready(function() {
+  $(".pborder2").click(function() {
+    $(".pborder2").removeClass("border1");
+    $(".pborder2").addClass("border1");
+  });
+});
